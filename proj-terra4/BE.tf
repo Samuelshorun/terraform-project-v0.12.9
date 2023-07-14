f2a8d8a94a1e95bf68e75bbc47a199d9a7a7db67
samuelshorun <samuelshorun@gmail.com>
2023-07-14 19:12:27 +0000

    more update on lab four

--- proj-terra4/BE.tf
+++ proj-terra4/BE.tf
@@ -4,7 +4,7 @@
     nsg web-nsg
     nic web-vm01*/
 
-    # terraform v12.29 used for this lab
+    # terraform v12.29 and Azure as provider is used for this lab
 
 resource "azurerm_resource_group" "be-rg" {
     name                        = "Be-rg"
@@ -71,6 +71,41 @@ resource "azurerm_network_interface_security_group_association" "be-rg" {
 resource "azure_virtual_machine" "be-rg" {
     name                        = "web-vm01"
     location                    = azurerm_resouce_group.be-rg.location
-    resource_group_name         = azurerm_resource_group
+    resource_group_name         = azurerm_resource_group.be-rg.name
+    network_interface_ids       = [azurerm_network_interface.be-rg.id]
+    vm_size                     = "Standard_B2s"
+
+    storage_image_reference { 
+        publisher                   = "MicrosoftWindowsServer"
+        offer                       = "WindowsServer"
+        sku                         = "2016-Datacenter"
+        version                     = "latest"
+    }
+    storage_os_disk {
+        name                        = "web-osdisk"
+        managed_disk_type           = "StandardSSD_LRS"
+        caching                     = "ReadWrite"
+        create_option               = "FromImage"
+    }
+    # definition of VM login propertise
+    os_profile {
+        computer_name               = "<Enter vm name>"
+        admin_username              = "<Enter admin name>"
+        admin_password              = "Enter vm password"
+    }
+    os_profile_windows_config {
+        enable_automatic_upgrades   = true
+        provision_vm_agent          = true
+
+    }
+  
+}
+
+resource "azurerm_virtual_machine_extension "be-rg" {
+    name                        = "iis-extention"
+    virtual_machine_id          = azurerm_virtual_machine.be-rg.id
+    publisher                   = "Microsoft.Compute"
+    type                        = "CustomScriptEtention"
+    tpye_hander_version          = ""
   
 }
